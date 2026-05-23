70da6fb6e7663b23379d39e558ae894371e6b3c6
GeleriuAdrian <ady04@proton.me>
2026-05-23 17:17:20 +0300

    Dotfiles modification

--- boot.nix
+++ boot.nix
@@ -8,10 +8,19 @@
       (pkgs.stdenv.mkDerivation {
         pname = "plymouth-theme-lone";
         version = "1.0";
-        src = "/home/ady/dotfiles/themes/lone";
+        src = ./themes/lone;
+        dontUnpack = true;
+
         installPhase = ''
-          mkdir -p $out/share/plymouth/themes
-          cp -r packs/lone $out/share/plymouth/themes/lone
+          # Create the exact directory structure Plymouth expects
+          mkdir -p $out/share/plymouth/themes/lone
+
+          # Copy all files directly into the theme folder
+          cp -r ./* $out/share/plymouth/themes/lone/
+
+          # Force the path in the config to be absolute so it doesn't get lost
+          sed -i 's|ImageDir=.*|ImageDir=/run/current-system/sw/share/plymouth/themes/lone|g' $out/share/plymouth/themes/lone/lone.plymouth
+          sed -i 's|ScriptFile=.*|ScriptFile=/run/current-system/sw/share/plymouth/themes/lone/lone.script|g' $out/share/plymouth/themes/lone/lone.plymouth
         '';
       })
     ];
