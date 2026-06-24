c2affd2636409c14d8e7be6b2edefa38b803f95d
GeleriuAdrian <ady04@proton.me>
2026-06-24 15:58:18 +0300

    gpu compatibility changes

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -1,10 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   hardware = {
     graphics = {
       enable = true;
       enable32Bit = true;
+      extraPackages = with pkgs; [
+        rocmPackages.clr.icd # OpenCL via ROCm
+        rocmPackages.clr # HIP runtime
+      ];
     };
     i2c.enable = true;
     bluetooth = {
@@ -12,6 +16,7 @@
       powerOnBoot = true;
       settings.Policy.AutoEnable = "true";
     };
+    amdgpu.opencl.enable = true;
   };
 
   services.blueman.enable = true;
@@ -19,4 +24,10 @@
 
   # hardware.i2c.enable = true;
   boot.kernelModules = [ "i2c-dev" ];
+
+  # Make sure ROCm knows about your GPU's target
+  environment.variables = {
+    ROC_ENABLE_PRE_VEGA = "1"; # not needed for 7700 XT but harmless
+    HSA_OVERRIDE_GFX_VERSION = "11.0.0"; # gfx1100 for RDNA3
+  };
 }
